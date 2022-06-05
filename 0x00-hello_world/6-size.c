04cadbdbacd6648252e3decd938c46809d4d80b6
NosieM <ayavuya96@gmail.com>
2022-06-05 06:17:12 -0700

    My c commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,10 +15,10 @@ long int l;
 long long int ll;
 float f;
 
-printf("Size of a char: %lu byte(s)\n", sizeof(c));
-printf("Size of an int: %lu bytes(s)\n", sizeof(i));
-printf("Size of long int: %lu byte(s)\n", sizeof(l));
-printf("Size of long long int: %lu byte(s)\n", sizeof(ll));
-printf("Size of float: %lu byte(s)\n", sizeof(f));
+printf("Size of a char: %lu byte(s)", sizeof(c));
+printf("Size of an int: %lu bytes(s)", sizeof(i));
+printf("Size of long int: %lu byte(s)", sizeof(l));
+printf("Size of long long int: %lu byte(s)", sizeof(ll));
+printf("Size of float: %lu byte(s)", sizeof(f));
 return (0);
 }
