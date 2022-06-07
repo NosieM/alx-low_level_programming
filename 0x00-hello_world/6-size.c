9715c8b5e44aeebfe38b5e177423a7d757c320cc
NosieM <ayavuya96@gmail.com>
2022-06-07 02:44:26 -0700

    My c commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,6 +17,6 @@ printf("Size of a char: %d byte(s)\n", (int)sizeof(c));
 printf("Size of an int : %d byte(s)\n", (int)sizeof(i));
 printf("Size of a long int: %d byte(s)\n", (int)sizeof(li));
 printf("Size of a long long int: %d byte(s)\n", (int)sizeof(lli));
-
+printf("Size of a float:%d byte(s)\n", (int)sizeof(f));
 return (0);
 }
