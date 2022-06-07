6cbb18bd15d9f244c659a0c60d8b74824d62ab48
NosieM <ayavuya96@gmail.com>
2022-06-07 16:13:32 -0700

    My c commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
-#include 'main.h'
+#include "main.h"
 
 /**
  * main - Print _putchar
  *
  * Return: Always 0 (success)
  */
+int _putchar(char c)
+{
 
+_putchar(c);
+return (0);
+}
