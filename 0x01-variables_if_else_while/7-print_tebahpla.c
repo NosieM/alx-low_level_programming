451b794d427c33f1375cf2a4ccf895c010e37ce7
NosieM <ayavuya96@gmail.com>
2022-06-11 15:27:02 -0700

    My c commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet reverse
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x;
+
+for (x = 'z' ; x >= 'a'; x--)
+putchar(x);
+putchar('\n');
+return (0);
+
+}
