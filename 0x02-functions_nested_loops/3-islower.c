5876d45268386076ab9b5e5c5c7cfc6945a7214e
NosieM <ayavuya96@gmail.com>
2022-06-12 17:41:29 -0700

    My c commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
