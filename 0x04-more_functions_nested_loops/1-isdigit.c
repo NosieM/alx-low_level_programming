77fc2959e569390cf5b984e411cb06b62f51240a
NosieM <ayavuya96@gmail.com>
2022-06-12 18:11:18 -0700

    My c commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+int _isdigit(int c)
+{
+if (c >= 0 && c <= 9)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
